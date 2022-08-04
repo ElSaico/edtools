172affdeee6d960a2857f1c843d8561e0d18b92a
Bruno Marques <bruno.oliveira_marques@siemens.com>
2022-08-04 17:52:06 +0100

    Basic homepage structure

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,115 +2,110 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
         <q-toolbar-title>
-          Quasar App
+          ED Tools
         </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
     <q-page-container>
-      <router-view />
+      <q-splitter
+        model-value="128px"
+      >
+        <template v-slot:before>
+          <q-tabs
+            v-model="tab"
+            vertical
+          >
+            <q-route-tab
+              icon=""
+              label="Pristines"
+              to="/pristine"
+              exact
+            />
+            <q-route-tab
+              icon=""
+              label="Rares"
+              to="/rares"
+              exact
+            />
+            <q-route-tab
+              icon=""
+              label="RES"
+              to="/res"
+              exact
+            />
+            <q-route-tab
+              icon=""
+              label="Distances"
+              to="/dist"
+              exact
+            />
+            <q-route-tab
+              icon=""
+              label="Miner's Tool"
+              to="/miner"
+              exact
+            />
+            <q-route-tab
+              icon=""
+              label="Sphere"
+              to="/sphere"
+              exact
+            />
+            <q-route-tab
+              icon=""
+              label="List"
+              to="/list"
+              exact
+            />
+            <q-route-tab
+              icon=""
+              label="Road to Riches"
+              to="/expl"
+              exact
+            />
+            <q-route-tab
+              icon=""
+              label="Selenium"
+              to="/selenium"
+              exact
+            />
+            <q-route-tab
+              icon=""
+              label="Massacre"
+              to="/pve"
+              exact
+            />
+            <q-route-tab
+              icon=""
+              label="Hotspots"
+              to="/hotspot"
+              exact
+            />
+          </q-tabs>
+        </template>
+        <template v-slot:after>
+          <router-view />
+        </template>
+      </q-splitter>
     </q-page-container>
   </q-layout>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
-import EssentialLink from 'components/EssentialLink.vue';
-
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
 
 export default defineComponent({
   name: 'MainLayout',
 
   components: {
-    EssentialLink
   },
 
   setup () {
-    const leftDrawerOpen = ref(false)
-
     return {
-      essentialLinks: linksList,
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
-    }
+      tab: ref('pristine')
+    };
   }
 });
 </script>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,15 +4,28 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: 'pristine', component: () => import('pages/Pristines.vue') },
+      { path: 'rares', component: () => import('pages/Rares.vue') },
+      { path: 'res', component: () => import('pages/RES.vue') },
+      { path: 'dist', component: () => import('pages/Distances.vue') },
+      { path: 'miner', component: () => import('pages/MinersTool.vue') },
+      { path: 'sphere', component: () => import('pages/Sphere.vue') },
+      { path: 'list', component: () => import('pages/List.vue') },
+      { path: 'expl', component: () => import('pages/RoadToRiches.vue') },
+      { path: 'selenium', component: () => import('pages/Selenium.vue') },
+      { path: 'pve', component: () => import('pages/Massacre.vue') },
+      { path: 'hotspot', component: () => import('pages/Hotspots.vue') }
+    ]
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue'),
-  },
+    component: () => import('pages/ErrorNotFound.vue')
+  }
 ];
 
 export default routes;
